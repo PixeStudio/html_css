aad5716301820b4068ea84df5ac137190406deb2
PixeStudio <56841910+PixeStudio@users.noreply.github.com>
2025-07-26 19:16:05 +0200

    feat: add modalManager class

---
+++ modalManager.js
@@ -0,0 +1,40 @@
+class modalManager {
+    constructor() {
+        this.modals = document.querySelectorAll('.modal');
+        this.init();
+    }
+
+    init() {
+        this.modals.forEach(modal => {
+            //close modal using x
+            const closeBtn = modal.querySelector('modal-close');
+            if(closeBtn) {
+                closeBtn.addEventListener('click', () => this.close(modal));
+            }
+        
+            //click far from modal content
+            modal.addEventListener('click', (e) => {
+                if(e.target === modal) this.close(modal);
+            }); 
+            
+            //close modal using ESC
+            document.addEventListener('keydown', (e) => {
+                if(e.key === "Escape") {
+                    this.modal.forEach(modal => this.close(modal));
+                }
+            });
+
+
+        });
+    }
+
+    open(modalId) {
+        document.getElementById(modalId).classList.add('active');
+    }
+
+    close(modal) {
+        modal.classList.remove('active');
+    }
+}
+
+window.modalManager = modalManager;
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     /*COOKIES MODAL*/
-    
+
     const cookieConsent = document.getElementById('cookieConsent');
     const acceptBtn = document.getElementById('acceptCookies');
 
-    if(localStorage.getItem('cookieAccepted') === '1') {
+    /*if(localStorage.getItem('cookieAccepted') === '1') {
         cookieConsent.classList.add('hide');
-    }
+    }*/
 
     acceptBtn.addEventListener('click', function() {
         cookieConsent.classList.add('hide');
